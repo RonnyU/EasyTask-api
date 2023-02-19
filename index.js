4ef4a4017c15b0d68c1a3ad2602f52d056a6d8b1
RonnyU <ronny_magdiel@outlook.com>
2023-02-19 00:34:16 -0600

    added typescript to the project

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.listen(4000, () => {
-  console.log('Server running at 4000 port');
-});
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_, res) => {
+  console.log('someone pinged here!!');
+  res.send('ping');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running ON PORT ${PORT}`);
+});
